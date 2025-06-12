68830a7405082fe66a53445a6c79d1cbd5200d5c
Alex Siegel <asiegel@jumptrading.com>
2025-06-12 13:38:23 +0000

    work

--- src/app/grpc-geyser/Local.mk
+++ src/app/grpc-geyser/Local.mk
@@ -2,21 +2,21 @@ PKG = /data/asiegel/pkg
 
 CPPFLAGS += -I$(PKG)/include -Wno-conversion -Wno-pedantic -Wno-unused-parameter
 
-LDFLAGS += -Wl,--start-group $(wildcard $(PKG)/lib/libgrpc*.a) -Wl,--end-group
-LDFLAGS += -Wl,--start-group $(wildcard $(PKG)/lib/libupb_*.a) -Wl,--end-group
-LDFLAGS += $(PKG)/lib/libgpr.a
-LDFLAGS += $(PKG)/lib/libaddress_sorting.a
-LDFLAGS += $(PKG)/lib/libcares.a
-LDFLAGS += $(PKG)/lib/libre2.a
-LDFLAGS += $(PKG)/lib/libssl.a
-LDFLAGS += $(PKG)/lib/libcrypto.a
-LDFLAGS += $(PKG)/lib/libz.a
-LDFLAGS += $(PKG)/lib64/libprotobufd.a
-LDFLAGS += -Wl,--start-group $(wildcard $(PKG)/lib64/libabsl_*.a) -Wl,--end-group
-LDFLAGS += $(PKG)/lib/libutf8_range_lib.a
-LDFLAGS += $(PKG)/lib64/libutf8_validity.a
-LDFLAGS += -pthread -ldl -lsystemd
+GRPC_LIBS += -Wl,--start-group $(wildcard $(PKG)/lib/libgrpc*.a) -Wl,--end-group
+GRPC_LIBS += -Wl,--start-group $(wildcard $(PKG)/lib/libupb_*.a) -Wl,--end-group
+GRPC_LIBS += $(PKG)/lib64/libprotobufd.a
+GRPC_LIBS += -Wl,--start-group $(wildcard $(PKG)/lib64/libabsl_*.a) -Wl,--end-group
+GRPC_LIBS += $(PKG)/lib/libgpr.a
+GRPC_LIBS += $(PKG)/lib/libaddress_sorting.a
+GRPC_LIBS += $(PKG)/lib/libcares.a
+GRPC_LIBS += $(PKG)/lib/libre2.a
+GRPC_LIBS += $(PKG)/lib/libssl.a
+GRPC_LIBS += $(PKG)/lib/libcrypto.a
+GRPC_LIBS += $(PKG)/lib/libz.a
+GRPC_LIBS += $(PKG)/lib/libutf8_range_lib.a
+GRPC_LIBS += $(PKG)/lib64/libutf8_validity.a
+GRPC_LIBS += -pthread -ldl -lsystemd
 
-$(call make-bin,fd_grpc_geyser,geyser_server geys_methods geys_filter geys_fd_loop geyser.grpc.pb geyser.pb solana-storage.pb,fd_discof fd_disco fd_flamenco fd_reedsol fd_funk fd_tango fd_choreo fd_waltz fd_ballet fd_util,$(SECP256K1_LIBS))
+$(call make-bin,fd_grpc_geyser,geyser_server geys_methods geys_filter geys_fd_loop geyser.grpc.pb geyser.pb solana-storage.pb,fd_discof fd_disco fd_flamenco fd_reedsol fd_funk fd_tango fd_choreo fd_waltz fd_ballet fd_util,$(SECP256K1_LIBS) $(GRPC_LIBS))
 
-$(call make-unit-test,test_geyser_client,test_geyser_client geyser.grpc.pb geyser.pb solana-storage.pb,fd_discof fd_disco fd_flamenco fd_reedsol fd_funk fd_tango fd_choreo fd_waltz fd_ballet fd_util,$(SECP256K1_LIBS))
+$(call make-unit-test,test_geyser_client,test_geyser_client geyser.grpc.pb geyser.pb solana-storage.pb,fd_discof fd_disco fd_flamenco fd_reedsol fd_funk fd_tango fd_choreo fd_waltz fd_ballet fd_util,$(SECP256K1_LIBS) $(GRPC_LIBS))
